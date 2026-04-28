7514bccc82573095ce9e5da0b9737444364f51ae
PracticeMedicine <practicemedicine3@gmail.com>
2026-04-29 02:10:28 +0800

    chore(memory): Separate LinearArena from Memory source files

--- Source/CTF/ctf/LinearArena.h
+++ Source/CTF/ctf/LinearArena.h
@@ -3,42 +3,43 @@
  * Copyright (c) 2026 The Aridity Team, all rights reserved.
  *
  * This file is part of the Cereon Template Framework project.
- * 
+ *
  * Cereon Template Framework is free software: you can redistribute
  * it and/or modify it under the terms of the GNU Lesser General
  * Public License as published by the Free Software Foundation, either
  * version 3 of the License, or any later version.
- * 
+ *
  * Cereon Template Framework is distributed in the hope that it will
  * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
- * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with Cereon Template Framework. If not, see <https://www.gnu.org/licenses/>. 
+ * along with Cereon Template Framework. If not, see <https://www.gnu.org/licenses/>.
  */
 
-#ifndef CTF_MEMORY_H
-#define CTF_MEMORY_H
+#ifndef CTF_LINEARARENA_H
+#define CTF_LINEARARENA_H
 #pragma once
 
-#include "CTF.h"
+namespace CTF
+{
 
-namespace CTF {
-
-class CTF_API Memory
+class CTF_API LinearArena
 {
 public:
-    static void *Alloc( size_t size );
-    static void *Calloc( size_t count, size_t size );
-    static void Free( void *ptr );
+    LinearArena( size_t size );
+    ~LinearArena();
 
-    static void *Realloc( void *ptr, size_t newSize );
+    void *Alloc( size_t size );
+    void Reset();
 
-    static void *AllocAligned( size_t size, size_t alignment );
-    static void FreeAligned( void *ptr );
+private:
+    char *m_buffer;
+    size_t m_capacity;
+    size_t m_offset;
 };
 
 }
 
-#endif // !CTF_MEMORY_H
+#endif // !CTF_LINEARARENA_H
